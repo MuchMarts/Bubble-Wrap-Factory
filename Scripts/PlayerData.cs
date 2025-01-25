5bf26148e501263b5aa81b42bc5583564dda77fa
MuchMarts <98103825+MuchMarts@users.noreply.github.com>
2025-01-25 15:34:09 +0200

    Global player data store

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+// Global class to store player data, persists between scenes
+
+public partial class PlayerData : Node
+{
+	public static PlayerData Instance { get; private set; }
+
+	public int HighScore { get; set; }
+
+	public int UpgradePoints { get; set; }
+
+	public Dictionary<UpgradeType, Upgrade> Upgrades { get; set; }
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Instance = this;
+	}
+}
+
+// Used to store the upgrade data in a readable format
+enum UpgradeType
+{
+	PopSize,
+	SheetCount,
+	SheetSpeed,
+	ShiftLength,
+}
+
+public class Upgrade
+{
+	public int Level { get; set; }
+	public int Cost { get; set; }
+	public int Value { get; set; }
+}
